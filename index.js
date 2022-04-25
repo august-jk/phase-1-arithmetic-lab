1ba8ba6b673729ed2d73b4d065b53248dba0980b
Aletha Wheeless <augustkama98@gmail.com>
2022-04-25 13:40:21 -1000

    commit tree cc43282c7cb9876bba982f01f4a6ecdc8d55b7c9

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 124;
+let num4 = 8;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = num3 % num4;
+let max = Math.max(7, 13, 5, 20);
